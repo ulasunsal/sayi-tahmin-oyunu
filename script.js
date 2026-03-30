2654e5aff7c0dffd6c4cad9bc73ece5b0b14e3e9
ulasunsal <ulas_unsal@hotmail.com>
2026-03-31 00:32:37 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
+let rastgeleSayi = Math.floor(Math.random() * 100) + 1;
 
+function kontrolEt() {
+    let tahmin = document.getElementById("tahmin").value;
+    let sonuc = document.getElementById("sonuc");
+
+    if (tahmin == rastgeleSayi) {
+        sonuc.innerText = "🎉 Doğru tahmin!";
+    } else if (tahmin < rastgeleSayi) {
+        sonuc.innerText = "Daha büyük bir sayı dene!";
+    } else {
+        sonuc.innerText = "Daha küçük bir sayı dene!";
+    }
+}
